062591a5508fb395d3778b21b1ca02bfc3caf1ae
Anthony Malary <amalary@bu.edu>
2022-02-16 14:58:55 -0800

    Finished question 1

---
+++ Solar-System-Lab.js
@@ -0,0 +1,15 @@
+const solarSystem = [{ name: "Mercury", ringSystem: false, moons: [] },
+{ name: "Venus", ringSystem: false, moons: [] },{ name: "Earth", ringSystem: false, moons: ["TheMoon"] }, 
+{ name: "Mars", ringSystem: false, moons: ["Phobos", "Deimos"] },
+{ name:"Jupiter", ringSystem: true, moons: ["Europa", "Ganymede", "Io", "Callisto"] },
+{ name:"Saturn", ringSystem: true, moons: ["Titan", "Enceladus", "Rhea", "Mimas"] }, 
+{ name:"Uranus", ringSystem: true, moons: ["Miranda", "Titania", "Ariel", "Umbriel"] },
+{ name: "Neptune", ringSystem: true, moons: ["Triton", "Nereid"] }]; 
+
+console.log(solarSystem[0].ringSystem);  
+
+
+// Question 1 
+
+console.log(solarSystem[4].moons);
+
